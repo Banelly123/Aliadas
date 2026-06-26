366ff64379eb251977f6abf4628bdf12ea12f8b1
Banelly123 <2330060@upt.edu.mx>
2026-06-26 00:21:10 -0600

    Update CommunityModule.kt

--- backend/src/main/kotlin/com/aliadas/community/CommunityModule.kt
+++ backend/src/main/kotlin/com/aliadas/community/CommunityModule.kt
@@ -6,6 +6,7 @@ import io.ktor.server.auth.jwt.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.application.*
 import io.ktor.http.*
 import kotlinx.serialization.Serializable
 import org.jetbrains.exposed.sql.*
